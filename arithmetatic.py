b878f890bff96b58524d94a13e51903ce7a10356
bhavububblu21 <archanaarchana991986@gmail.com>
2025-11-10 16:43:32 +0530

    Create arithmetatic.py

---
+++ arithmetatic.py
@@ -0,0 +1,13 @@
+a=int( input("enter the age"))
+b=int(input(" enter the name "))
+weight=float(input("enter the weight"))
+print("age is",a)
+print("name is",b)
+print("weight is",weight)
+addition=a+weight
+sub=a-weight
+divi1=a/weight
+divi2=a//weight
+divi3=a%weight
+multi=a*weight
+print(addition,sub,divi1,divi2,divi3,multi)
