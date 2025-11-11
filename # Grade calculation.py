c83a6382501a012ddd73b6e65f520b3ca7a48d05
bhavububblu21 <archanaarchana991986@gmail.com>
2025-11-11 14:54:40 +0530

    Create # Grade calculation.py

---
+++ # Grade calculation.py
@@ -0,0 +1,10 @@
+grade calculation
+marks=int(input("enter the marks"))
+if marks>=90:
+  print ("grade A")
+elif marks>=75:
+  print("grade B")
+elif marks>=50:
+  print("grade C")
+else:
+  print("fail")
